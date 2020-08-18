f55bf58a6d05ed80f043bea0eb54561dd5c86ea9
Trever O <toveson9522@gmail.com>
2020-08-18 17:09:51 -0600

    added styles to .html and pseudo code to .js

---
+++ planner.js
@@ -0,0 +1,11 @@
+// I need to get the current time first
+    // use moment
+    
+// with the current time I need to change the colors of the text area 
+    // if else statements
+        // if current hour = time block then add present css class to that clock
+        // else/if current hour > time block add past css class to time block
+        // else curruent hour < time block add future css class to time block
+
+// I need to pull any local storage to its time block when the page loads
+    // I need to push any added time block info to local storage when the user clicks the save button

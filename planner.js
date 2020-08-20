9f35383fe7555cdc156e039c2b0f29a58512aa1c
Trever O <toveson9522@gmail.com>
2020-08-20 00:32:51 -0600

    textara now changes color properly

--- planner.js
+++ planner.js
@@ -4,7 +4,7 @@ var now = moment();
 var dayOfWeek = moment().format('dddd');
 var month = moment().format('MMMM');
 var date = moment().format('DD')
-var hour = moment().format('h');
+var hour = moment().format('HH');
 
 
 //  day of week, month, date
@@ -14,27 +14,27 @@ $('#currentDay').html(dayOfWeek + ', ' + month + ' ' + date);
 // I need to get the hour from html and use it to change the textarea
 // this gets only the number text from the time block
 
-var timeBlock9 = $("#9Block").text().replace(/\D/g, '');
-var timeBlock10 = $('#10Block').text().replace(/\D/g, '');
-var timeBlock11 = $('#11Block').text().replace(/\D/g, '');
-var timeBlock12 = $('#12Block').text().replace(/\D/g, '');
-var timeBlock1 = $('#1Block').text().replace(/\D/g, '');
-var timeBlock2 = $('#2Block').text().replace(/\D/g, '');
-var timeBlock3 = $('#3Block').text().replace(/\D/g, '');
-var timeBlock4 = $('#4Block').text().replace(/\D/g, '');
-var timeBlock5 = $('#5Block').text().replace(/\D/g, '');
+var timeBlock9 = parseInt($("#9Block").text().replace(/\D/g, ''));
+var timeBlock10 = parseInt($('#10Block').text().replace(/\D/g, ''));
+var timeBlock11 = parseInt($('#11Block').text().replace(/\D/g, ''));
+var timeBlock12 = parseInt($('#12Block').text().replace(/\D/g, ''));
+var timeBlock1 = parseInt($('#1Block').text().replace(/\D/g, '')) + 12;
+var timeBlock2 = parseInt($('#2Block').text().replace(/\D/g, '')) + 12;
+var timeBlock3 = parseInt($('#3Block').text().replace(/\D/g, '')) + 12;
+var timeBlock4 = parseInt($('#4Block').text().replace(/\D/g, '')) + 12;
+var timeBlock5 = parseInt($('#5Block').text().replace(/\D/g, '')) + 12;
 
 let hours = [timeBlock9, timeBlock10, timeBlock11, timeBlock12, timeBlock1, timeBlock2, timeBlock3, timeBlock4, timeBlock5];
 
+
 // with the current time I need to change the colors of the text area 
 // if else statements
-
 console.log(hour);
 function colorChange() {
-    if (parseInt(hour) === parseInt(hours)) {
+    if (parseInt(hour) === hours) {
         // if current hour = time block then add present css class to that clock
         $("textarea").addClass("present");
-    } else if (parseInt(hour) > parseInt(hours)) {
+    } else if (parseInt(hour) > hours) {
         // else/if current hour > time block add past css class to time block
         $("textarea").addClass("past");
     } else {

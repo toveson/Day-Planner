3b0a8935de8c9a8a5a2899039e94a34f946192c2
Trever O <toveson9522@gmail.com>
2020-08-20 22:06:39 -0600

    times are now really updating properly (I checked this time)

--- planner.js
+++ planner.js
@@ -25,44 +25,42 @@ var timeBlock4 = parseInt($('#4Block').text().replace(/\D/g, '')) + 12;
 var timeBlock5 = parseInt($('#5Block').text().replace(/\D/g, '')) + 12;
 
 let hours = [timeBlock9, timeBlock10, timeBlock11, timeBlock12, timeBlock1, timeBlock2, timeBlock3, timeBlock4, timeBlock5];
-
+// changes the current hour to a number
+// let hourNumb = parseInt(hour);
 
 // with the current time I need to change the colors of the text area 
 // if else statements
-console.log(hour);
-function colorChange() {
-    if (parseInt(hour) === hours) {
+// console.log(hourNumb);
+var text = $('textarea')
+function colorChange(currentIndex) {
+   
+    if (hour === hours[currentIndex]) {
         // if current hour = time block then add present css class to that clock
-        $("textarea").addClass("present");
-    } else if (parseInt(hour) > hours) {
+        text[currentIndex].classList.add("present");
+    } else if (hour > hours[currentIndex]) {
         // else/if current hour > time block add past css class to time block
-        $("textarea").addClass("past");
+       text[currentIndex].classList.add("past");
     } else {
         // else curruent hour < time block add future css class to time block
-        $("textarea").addClass("future");
+       text[currentIndex].classList.add("future");
     }
 };
 
-for (var i = 0; i < hours.length;) {
-    colorChange();
-    i++;
-    console.log(hours.length);
+for (var i = 0; i < text.length; i++) {
+    colorChange(i);
+    // console.log(text[i]);
 };
 
 // I need to pull any local storage to its time block when the page loads
 // I need to push any added time block info to local storage when the user clicks the save button
 // I need button listeners to save data
 // button event listeners
-$(".saveBtn").click();
-                // $("#9Btn, #10Btn, #11Btn, #12Btn, #13Btn, #14Btn, #15Btn, #16Btn, #17Btn").click();
-                // $("#10Btn").click();
-                // $("#11Btn").click();
-                // $("#12Btn").click();
-                // $("#13Btn").click();
-                // $("#14Btn").click();
-                // $("#15Btn").click();
-                // $("#16Btn").click();
-                // $("#17Btn").click();
+    
+    $('Btn9').on('click', function () {
+        var text9am = $('#text9').val();
+        console.log(text9am)
+        localStorage.setItem('9am', text9am)
+    });
 
 
 

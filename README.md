cdd21b55d8884173a328f280e2e2d32d092513e2
Trever O <toveson9522@gmail.com>
2020-08-21 19:54:52 -0600

    updated README.md

--- README.md
+++ README.md
@@ -1,56 +1,11 @@
 # Day-Planner
 
+This is a simple day planner for a normal work day from 9am to 5 pm. It allows the user to save events in the center column by entering what they would like and clicking the save button to the right. The users saved items will still be there even if they refresh the page.
 
+![Day Planner](./immages/dayPlanner.PNG)
 
-<!-- 
-# 05 Third-Party APIs: Work Day Scheduler
+this [Day Planner](https://toveson.github.io/Day-Planner/) is hosted on Github pages.
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+This application was authored by [Trever Oveson](https://github.com/toveson).
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-## User Story
-
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-    ----------THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-    ----------THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-    ----------THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-    THEN I can enter an event
-WHEN I click the save button for that timeblock
-    THEN the text for that event is saved in local storage
-WHEN I refresh the page
-    THEN the saved events persist
-```
-
-The following animation demonstrates the application functionality:
-
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved. -->
-
-
-
-
-HUGE thank you to Chrisopher Sannar for helping me with my color change function!
+HUGE thank you to Chrisopher Sannar for helping me wrap my head around some new concepts for color change function!
--- planner.js
+++ planner.js
@@ -1,19 +1,14 @@
-// I need to get the current time first
-// use moment
+// moment.js variables 
 var now = moment();
 var dayOfWeek = moment().format('dddd');
 var month = moment().format('MMMM');
 var date = moment().format('DD')
 var hour = moment().format('HH');
 
-
 //  day of week, month, date
 $('#currentDay').html(dayOfWeek + ', ' + month + ' ' + date);
 
-
-// I need to get the hour from html and use it to change the textarea
-// this gets only the number text from the time block
-
+// these gets only the number text from the time block and makes it a number
 var timeBlock9 = parseInt($("#9Block").text().replace(/\D/g, ''));
 var timeBlock10 = parseInt($('#10Block').text().replace(/\D/g, ''));
 var timeBlock11 = parseInt($('#11Block').text().replace(/\D/g, ''));
@@ -26,103 +21,79 @@ var timeBlock5 = parseInt($('#5Block').text().replace(/\D/g, '')) + 12;
 
 let hours = [timeBlock9, timeBlock10, timeBlock11, timeBlock12, timeBlock1, timeBlock2, timeBlock3, timeBlock4, timeBlock5];
 
-// Getting local storage and displaying it in the textarea
-
-
-
-
-// with the current time I need to change the colors of the text area 
-// if else statements
-
+// adds a class to the textarea depending on the time
 var text = $('textarea')
 function colorChange(currentIndex) {
-   
+
     if (hour == hours[currentIndex]) {
         // if current hour = time block then add present css class to that clock
         text[currentIndex].classList.add("present");
     } else if (hour > hours[currentIndex]) {
         // else/if current hour > time block add past css class to time block
-       text[currentIndex].classList.add("past");
+        text[currentIndex].classList.add("past");
     } else {
         // else curruent hour < time block add future css class to time block
-       text[currentIndex].classList.add("future");
+        text[currentIndex].classList.add("future");
     }
 };
 
 for (var i = 0; i < text.length; i++) {
     colorChange(i);
-    // console.log(text[i]);
 };
 
-// I need to pull any local storage to its time block when the page loads
-// button event listeners
-// I need to push any added time block info to local storage when the user clicks the save button
-// I need button listeners to save data
-
-
 // Button listener to save user input to local storage
-$( "#Btn9" ).click(function() {
+$("#Btn9").click(function () {
     let textBlock9 = $("#text9").val();
     localStorage.setItem('text9', textBlock9);
-  });
-// load local storage to textarea
-  $( "#text9" ).append(localStorage.getItem('text9'));
+});
 
-  $( "#Btn10" ).click(function() {
+$("#Btn10").click(function () {
     let textBlock10 = $("#text10").val();
     localStorage.setItem('text10', textBlock10);
-  });
+});
 
-  $( "#text10" ).append(localStorage.getItem('text10'));
-  
-  $( "#Btn11" ).click(function() {
+$("#Btn11").click(function () {
     let textBlock11 = $("#text11").val();
     localStorage.setItem('text11', textBlock11);
-  });
+});
 
-  $( "#text11" ).append(localStorage.getItem('text11'));
-  
-  $( "#Btn12" ).click(function() {
+$("#Btn12").click(function () {
     let textBlock12 = $("#text12").val();
     localStorage.setItem('text12', textBlock12);
-  });
+});
 
-  $( "#text12" ).append(localStorage.getItem('text12'));
-
-
-  $( "#Btn13" ).click(function() {
+$("#Btn13").click(function () {
     let textBlock13 = $("#text13").val();
     localStorage.setItem('text13', textBlock13);
-  });
+});
 
-  $( "#text13" ).append(localStorage.getItem('text13'));
-
-  $( "#Btn14" ).click(function() {
+$("#Btn14").click(function () {
     let textBlock14 = $("#text14").val();
     localStorage.setItem('text14', textBlock14);
-  });
+});
 
-  $( "#text14" ).append(localStorage.getItem('text14'));
-
-
-  $( "#Btn15" ).click(function() {
+$("#Btn15").click(function () {
     let textBlock15 = $("#text15").val();
     localStorage.setItem('text15', textBlock15);
-  });
+});
 
-  $( "#text15" ).append(localStorage.getItem('text15'));
-
-
-  $( "#Btn16" ).click(function() {
+$("#Btn16").click(function () {
     let textBlock12 = $("#text16").val();
     localStorage.setItem('text16', textBlock12);
-  });
+});
 
-  $( "#text16" ).append(localStorage.getItem('text16'));
-
-  $( "#Btn17" ).click(function() {
+$("#Btn17").click(function () {
     let textBlock12 = $("#text17").val();
     localStorage.setItem('text17', textBlock12);
-  });
+});
 
-  $( "#text17" ).append(localStorage.getItem('text17'));
+// load local storage to textarea
+$("#text9").append(localStorage.getItem('text9'));
+$("#text10").append(localStorage.getItem('text10'));
+$("#text11").append(localStorage.getItem('text11'));
+$("#text12").append(localStorage.getItem('text12'));
+$("#text13").append(localStorage.getItem('text13'));
+$("#text14").append(localStorage.getItem('text14'));
+$("#text15").append(localStorage.getItem('text15'));
+$("#text16").append(localStorage.getItem('text16'));
+$("#text17").append(localStorage.getItem('text17'));
